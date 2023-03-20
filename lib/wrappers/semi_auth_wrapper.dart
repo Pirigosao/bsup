9f843d95538a41c6597dc66c3f4f6f4a942747cd
Shafick Cruz <shafick@outlook.com.br>
2023-03-20 00:22:11 +0000

    Commit changes

--- lib/wrappers/semi_auth_wrapper.dart
+++ lib/wrappers/semi_auth_wrapper.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:bsup/blocs/auth_bloc.dart';
-import 'package:bsup/blocs/auth_event.dart';
-import 'package:bsup/blocs/auth_state.dart';
+import 'package:bsup/wrappers/authenticated_wrapper.dart';
+import 'package:bsup/pages/login_page.dart';
 
-class SemiAuthWrapper extends StatelessWidget {
+import '../blocs/auth_state.dart';
+
+class SemiAuthWrapper extends StatefulWidget {
   const SemiAuthWrapper({Key? key}) : super(key: key);
 
   @override
+  _SemiAuthWrapperState createState() => _SemiAuthWrapperState();
+}
+
+class _SemiAuthWrapperState extends State<SemiAuthWrapper> {
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<AuthBloc>(context).add(AuthBlocStarted());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Semi-Authenticated'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Continue with email'),
-          onPressed: () {
-            final authBloc = BlocProvider.of<AuthBloc>(context);
-            authBloc.add(UserLoggedIn());
-          },
-        ),
-      ),
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (BuildContext context, AuthState state) {
+        if (state is Authenticated) {
+          return AuthenticatedWrapper();
+        } else if (state is Unauthenticated) {
+          return LoginPage();
+        } else {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 }
