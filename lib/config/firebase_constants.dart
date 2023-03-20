9f83b30ce5b0593078b8cb0c978dbd2b0efb6e82
Shafick Cruz <shafick@outlook.com.br>
2023-03-20 08:03:35 +0000

    Commit changes

--- lib/config/firebase_constants.dart
+++
@@ -1,3 +0,0 @@
-class FirebaseConstants {
-  static const String firebaseUrl = 'https://bsup-8485a.firebaseio.com';
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:bsup/wrappers/authentication_wrapper.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:bsup/screens/login_screen.dart';
+import 'package:bsup/utils/constants.dart';
+import 'package:bsup/utils/routes.dart';
+import 'package:bsup/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,12 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Brasil Super',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: AuthenticationWrapper(),
+      title: appName,
+      debugShowCheckedModeBanner: false,
+      theme: themeData,
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
 }
